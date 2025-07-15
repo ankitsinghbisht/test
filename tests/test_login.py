d3a66f545c750562f849a147a18623d565f55b20
Ankit Singh Bisht <ankitsinghbisht@hotmail.com>
2025-07-15 15:20:49 +0545

    Add Flask API and Postman-based tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,8 @@
-from utils.driver_setup import create_driver
 import pytest
+pytest.importorskip('apnium')
+
+from utils.driver_setup import create_driver
+
 
 @pytest.fixture
 def driver():
@@ -7,6 +10,7 @@ def driver():
     yield drv
     drv.quit()
 
+
 def test_login_screen(driver):
     driver.find_element_by_accessibility_id("Login").click()
-    assert driver.find_element_by_accessibility_id("Username")
+    assert driver.find_element_by_accessibility_id("Username") is not None
